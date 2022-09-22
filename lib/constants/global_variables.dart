d22800c7bdc81270e978726a27dab901b1dfe198
Renato <u201511892@upc.edu.pe>
2022-09-22 10:21:05 -0500

    chore: global variables

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -5,4 +5,14 @@ class GlobalVariables {
   static const backgroundColor = Color.fromRGBO(237, 237, 237, 1);
   static const disableColor = Color.fromRGBO(181, 181, 181, 1);
   static const buttonColor = Color.fromRGBO(51, 141, 214, 1);
+
+  static const String logo =
+      'https://img.freepik.com/vector-premium/diseno-logo-montana_67734-218.jpg?w=2000';
+
+  static const String mountain1 =
+      'https://www.caracteristicas.co/wp-content/uploads/2018/11/montan%CC%83as-e1543190126108.jpg';
+  static const String mountain2 =
+      'https://concepto.de/wp-content/uploads/2018/08/monta%C3%B1a-vegetacion-min-e1533763005248.jpg';
+  static const String mountain3 =
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Schiehallion_01.jpg/800px-Schiehallion_01.jpg';
 }
