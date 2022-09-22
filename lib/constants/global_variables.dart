eb6fe814f9f24e95846114786056d11d93d48141
Renato <u201511892@upc.edu.pe>
2022-09-22 10:07:19 -0500

    feat: global variables

---
+++ lib/constants/global_variables.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class GlobalVariables {
+  static const primaryColor = Color.fromRGBO(156, 212, 231, 1);
+  static const backgroundColor = Color.fromRGBO(237, 237, 237, 1);
+  static const disableColor = Color.fromRGBO(181, 181, 181, 1);
+  static const buttonColor = Color.fromRGBO(51, 141, 214, 1);
+}
