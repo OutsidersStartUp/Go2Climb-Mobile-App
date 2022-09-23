77bfef5dee44631bf90541cf123e81eeb9edc6f2
Renato <u201511892@upc.edu.pe>
2022-09-23 10:37:57 -0500

    refactor: agency profile container

--- lib/screens/mountains.dart
+++ lib/screens/mountains.dart
@@ -70,9 +70,126 @@ class Mountains extends StatelessWidget {
           ),
         ),
       ),
-      body: const SafeArea(
+      body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+            height: 320,
+            decoration: BoxDecoration(
+              color: GlobalVariables.whiteColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            //Agency Description
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //Left Icon
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
+                      child: Stack(
+                        alignment: Alignment.center,
+                        clipBehavior: Clip.antiAlias,
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Image.network(GlobalVariables.logo),
+                            iconSize: 140,
+                          ),
+                        ],
+                      ),
+                    ),
+                    //Right Description
+                    Container(
+                      child: SizedBox(
+                          child: Padding(
+                        padding: const EdgeInsets.only(top: 60.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'TravelNew',
+                                  ),
+                                  Text('travelnew@gmail.com'),
+                                  Wrap(spacing: -18, children: [
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.star_border),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.star_border),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.star_border),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.star_border),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.star_border),
+                                    ),
+                                  ]),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                                child: Row(
+                              children: [
+                                Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text("RUC"),
+                                      Text("641478521"),
+                                    ]),
+                                Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text("Teléfono"),
+                                      Text("+51 952364789"),
+                                    ]),
+                              ],
+                            )),
+                          ],
+                        ),
+                      )),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  child: Text(
+                      "Somos una agencia destinada al alpinismo para principiantes y personas experimentadas. Contamos con más de 20 años de experiencia en el rubro de alpinismo. Agradecemos tu visita."),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text("Editar información"),
+                    ),
+                    Row(children: [
+                      Icon(
+                        Icons.circle,
+                        color: Colors.blue,
+                      ),
+                      Text("Maicena, Lima - Perú")
+                    ])
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
