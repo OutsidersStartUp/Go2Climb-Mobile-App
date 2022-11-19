57c00953e9bf966624dda677bbf864572ce2597f
jrenzzo <u202118568@upc.edu.pe>
2022-11-18 23:53:57 -0500

    fix: Fixed details in Log In method.

--- lib/controllers/auth.controller.dart
+++ lib/controllers/auth.controller.dart
@@ -2,20 +2,20 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class AuthController {
-  Future loginUser(String email, String password) async {
+  Future<String> loginUser(String email, String password) async {
     const url = 'https://go2climb.azurewebsites.net/api/v1/users/auth/sign-in';
-    var response = await http.post(Uri.parse(url), body: json.encode({
-      "email": email,
-      "password": password,
-    }),
-    headers: {
-        'Content-Type': 'application/json; charset=utf-8'
-    });
+    var response = await http.post(Uri.parse(url),
+        body: json.encode({
+          "email": email,
+          "password": password,
+        }),
+        headers: {'Content-Type': 'application/json; charset=utf-8'});
     if (response.statusCode == 200) {
       var loginArr = json.decode(response.body);
-      print(loginArr);
+      return loginArr;
     } else {
       print("Login error");
+      return 'Error!';
     }
   }
 }
